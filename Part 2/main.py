5158d4892e9543b2da650ee8a4d27fe5414b18fb
Selcuk Aksoy <111302175+Cukowski@users.noreply.github.com>
2025-01-17 03:24:44 +0100

    Part 2

---
+++ Part 2/main.py
@@ -0,0 +1,89 @@
+import pygame
+import constants
+from character import Character
+
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption("My Game")
+
+# Create clock for FPS
+clock = pygame.time.Clock()
+
+# Define player movements
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
+
+# Scale function
+def scale_image(image, scale):
+    return pygame.transform.scale(image, (image.get_width() * scale, image.get_height() * scale))
+
+animated_player_images = []
+for i in range(4):
+    player_image = pygame.image.load(f"assets/character_0/{i}.png")
+    player_image = scale_image(player_image, constants.SCALE)
+    animated_player_images.append(player_image)
+
+# Create character
+character = Character(100, 100, animated_player_images)
+
+# Main game loop
+running = True
+while running:
+    # Set FPS
+    clock.tick(constants.FPS)
+
+    # Clear screen
+    screen.fill(constants.BACKGROUND_COLOR)
+
+    # Calculate characters movement delta x and delta y
+    deltaX = 0
+    deltaY = 0
+    if moving_left:
+        deltaX -= constants.SPEED
+    if moving_right:
+        deltaX += constants.SPEED
+    if moving_up:
+        deltaY -= constants.SPEED
+    if moving_down:
+        deltaY += constants.SPEED
+
+    # Update character position
+    character.move(deltaX, deltaY)
+
+    # Update character
+    character.update()
+
+    # Draw character on screen
+    character.draw(screen)
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # keyboard inputs
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            elif event.key == pygame.K_RIGHT:
+                moving_right = True
+            elif event.key == pygame.K_UP:
+                moving_up = True
+            elif event.key == pygame.K_DOWN:
+                moving_down = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            elif event.key == pygame.K_RIGHT:
+                moving_right = False
+            elif event.key == pygame.K_UP:
+                moving_up = False
+            elif event.key == pygame.K_DOWN:
+                moving_down = False
+
+    # Update screen
+    pygame.display.update()
+
+pygame.quit()
+exit()
